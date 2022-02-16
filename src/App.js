02e9d101f7d61a712c7893fce36bdfa0ebea1ea6
unknown <ceo.ahmedsalah@gmail.com>
2022-02-17 01:14:51 +0200

    main files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TodoList from '../src/Todolist';
+function App() {
+  return (
+    <div className="todo-app"> 
+    <TodoList />
+    </div>
+  );
+}
+
+export default App;
